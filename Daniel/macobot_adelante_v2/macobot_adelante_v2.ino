6995b7f15359ac04683799f7c80a5db428dd237b
Daniel Hernández <deahernandezc@correo.udistrital.edu.co>
2021-05-09 20:04:57 -0500

    Actualización Marcha adelante
    subo código arreglado para ir hacia adelante

--- Daniel/macobot_adelante_v2/macobot_adelante_v2.ino
+++ Daniel/macobot_adelante_v2/macobot_adelante_v2.ino
@@ -13,14 +13,11 @@ Servo c_i_t;
 Servo r_i_t;
 
 int ang_c_d[] = {135, 120, 105, 90, 75, 60, 45, 60, 75, 90, 105, 120};
-int ang_r_d[] = {  0,  0,  0,  0,  0,  90, 90, 90, 90, 90, 90, 90};
+int ang_r_d[] = {  0, 15, 30, 45, 60, 75, 90, 75, 60, 45, 30, 15};
 int ang_c_i[] = { 45, 60, 75, 90, 105, 120, 135, 120, 105, 90, 75, 60};
-int ang_r_i[] = {180, 180, 180, 180, 180, 90, 90, 90, 90, 90, 90, 90};
-int swich = 1 ;
+int ang_r_i[] = {180, 165, 150, 135, 120, 105, 90, 105, 120, 135, 150, 165};
 
 void setup() {
-  pinMode(10, INPUT);
-  
   Serial.begin(9600);
   c_d_f.attach(4);
   r_d_f.attach(5);
@@ -30,32 +27,52 @@ void setup() {
   r_d_t.attach(6);
   c_i_t.attach(8);
   r_i_t.attach(9);
+  // posiciònes iniciales
+  c_d_f.write(ang_c_d[0]);
+  r_d_f.write(ang_r_d[0]);
+  c_i_f.write(ang_c_i[0]);
+  r_i_f.write(ang_r_i[0]);
+  c_d_t.write(ang_c_d[0]);
+  r_d_t.write(ang_r_d[0]);
+  c_i_t.write(ang_c_i[0]);
+  r_i_t.write(ang_r_i[0]);
 }
 
 void loop() {
-  swich = digitalRead(10);
-  if (swich == HIGH) {
-    // primer movimiento diagonal
-    for (byte i = 0; i < 12; i = i + 1) {
+  // primer movimiento diagonal
+  for (byte i = 0; i < 20; i = i + 1) {
+    if (i < 12) {
       c_d_f.write(ang_c_d[i]);  // enviar posición al servo de la cadera derecha
       r_d_f.write(ang_r_d[i]);  // enviar posición al servo de la rodilla derecha
-      delay(50); // Pausa para cada paso
     }
-    for (byte i = 0; i < 12; i = i + 1) {
-      c_i_t.write(ang_c_i[i]);  // enviar posición al servo de la cadera izquierda
-      r_i_t.write(ang_r_i[i]);  // enviar posición al servo de la rodilla izquierda
-      delay(50); // Pausa para cada paso
+    if (i > 2) && (i < 14) {
+      c_i_t.write(ang_c_i[i - 2]); // enviar posición al servo de la cadera izquierda
+      r_i_t.write(ang_r_i[i - 2]); // enviar posición al servo de la rodilla izquierda
     }
-    // segundo movimiento diagonal
-    for (byte i = 0; i < 12; i = i + 1) {
-      c_i_f.write(ang_c_i[i]);  // enviar posición al servo de la cadera izquierda
-      r_i_f.write(ang_r_i[i]);  // enviar posición al servo de la rodilla izquierda
-      delay(50); // Pausa para cada paso
+    if (i > 5) && (i < 17) {
+      c_i_f.write(ang_c_i[i - 5]); // enviar posición al servo de la cadera izquierda
+      r_i_f.write(ang_r_i[i - 5]); // enviar posición al servo de la rodilla izquierda
     }
-    for (byte i = 0; i < 12; i = i + 1) {
-      c_d_t.write(ang_c_d[i]);  // enviar posición al servo de la cadera derecha
-      r_d_t.write(ang_r_d[i]);  // enviar posición al servo de la rodilla derecha
-      delay(50); // Pausa para cada paso
+    if (i > 8) {
+      c_d_t.write(ang_c_d[i - 8]); // enviar posición al servo de la cadera derecha
+      r_d_t.write(ang_r_d[i - 8]); // enviar posición al servo de la rodilla derecha
     }
+    delay(1000); // Pausa para cada paso
   }
+  //  for (byte i = 0; i < 12; i = i + 1) {
+  //    c_i_t.write(ang_c_i[i]);  // enviar posición al servo de la cadera izquierda
+  //    r_i_t.write(ang_r_i[i]);  // enviar posición al servo de la rodilla izquierda
+  //    //delay(1000); // Pausa para cada paso
+  //  }
+  //  // segundo movimiento diagonal
+  //  for (byte i = 0; i < 12; i = i + 1) {
+  //    c_i_f.write(ang_c_i[i]);  // enviar posición al servo de la cadera izquierda
+  //    r_i_f.write(ang_r_i[i]);  // enviar posición al servo de la rodilla izquierda
+  //    //delay(1000); // Pausa para cada paso
+  //  }
+  //  for (byte i = 0; i < 12; i = i + 1) {
+  //    c_d_t.write(ang_c_d[i]);  // enviar posición al servo de la cadera derecha
+  //    r_d_t.write(ang_r_d[i]);  // enviar posición al servo de la rodilla derecha
+  //    //delay(1000); // Pausa para cada paso
+  //  }
 }
