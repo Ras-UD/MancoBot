ab143eee5deea38af52b8f7849cf7e913e07808a
Daniel Hernández <deahernandezc@correo.udistrital.edu.co>
2021-05-18 10:52:46 -0500

    Mejora de código
    Ahora si incorporé una mejora del código simplificando las funciones pataPos() y pataNeg() a una sola función pata()

--- Codigos Individuales/Daniel/mancobot_v5_mov_izq.ino/mancobot_v5_mov_izq.ino.ino
+++ Codigos Individuales/Daniel/mancobot_v5_mov_izq.ino/mancobot_v5_mov_izq.ino.ino
@@ -13,7 +13,7 @@ Servo traDerCodo;
 Servo traIzq;
 Servo traIzqCodo;
 int swich = 1 ;
-int i = 0;
+int i, j = 0;
 
 int paso = 50;
 
@@ -21,7 +21,7 @@ void setup() {
   //declaración
   Serial.begin(9600);
   pinMode(10, INPUT);
-  
+
   delaIzqCodo.attach(2);
   delaIzq.attach(3);
   delaDer.attach(4);
@@ -30,7 +30,7 @@ void setup() {
   traIzq.attach(7);
   traDer.attach(8);
   traDerCodo.attach(9);
-  
+
   //inicializacion
   delaIzqCodo.write(0);   // 0 abajo
   delaIzq.write(90);        //90 AL MEDIO
@@ -44,120 +44,104 @@ void setup() {
 }
 
 //x=angulo inicial y= angulo final z=numero de la pata
-void pataPos(int x, int y, int z, int w) {
-  for (i = x; i <= y; i++)  //patas 4 y 5 arriba
-  {
-    switch (z) {
-      case 1 :
-        delaIzqCodo.write(i);
-        break;
-      case 2 :
-        delaIzq.write(i);
-        break;
-      case 3 :
-        delaDer.write(i);
-        break;
-      case 4 :
-        delaDerCodo.write(i);
-        break;
-      case 5 :
-        traIzqCodo.write(i);
-        break;
-      case 6 :
-        traIzq.write(i);
-        break;
-      case 7 :
-        traDer.write(i);
-        break;
-      case 8 :
-        traDerCodo.write(i);
-        break;
-    }
-    delay(w);
-  }
-}
-void pataNeg(int x, int y, int z, int w) {
 
-  for (i = x; i >= y; i--)  //patas 4 y 5 arriba
-  {
-    switch (z) {
-      case 1 :
-        delaIzqCodo.write(i);
-        break;
-      case 2 :
-        delaIzq.write(i);
-        break;
-      case 3 :
-        delaDer.write(i);
-        break;
-      case 4 :
-        delaDerCodo.write(i);
-        break;
-      case 5 :
-        traIzqCodo.write(i);
-        break;
-      case 6 :
-        traIzq.write(i);
-        break;
-      case 7 :
-        traDer.write(i);
-        break;
-      case 8 :
-        traDerCodo.write(i);
-        break;
-    }
-    delay(w);
-  }
-}
-
-void rotIzq() {
-  //levanta las patas 1 y 8
-  pataPos(0, 45, 1, paso);
-  pataPos(0, 45, 8, paso);
-  delay(150);
+void pata(int x, int y, int z, int w) {
   
+  if (x >= y) {
+    i = -x;
+    j = -y;
+  } else {
+    i = x;
+    j = y;
+  }
+  
+  for (i; i <= j; i++) //patas 4 y 5 arriba
+  {
+    switch (z) {
+      case 1 :
+        delaIzqCodo.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 2 :
+        delaIzq.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 3 :
+        delaDer.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 4 :
+        delaDerCodo.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 5 :
+        traIzqCodo.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 6 :
+        traIzq.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 7 :
+        traDer.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+      case 8 :
+        traDerCodo.write(abs(i));
+        //Serial.print(abs(i) + 180 * (z-1));
+        break;
+    }
+    //Serial.println();
+    delay(w);
+  }
+}
+
+void movIzq() {
+  //levanta las patas 1 y 8
+  pata(0, 45, 1, paso);
+  pata(0, 45, 8, paso);
+  delay(150);
+
   //movimiento hacia izquierda de hombros 3 y 6
-  pataNeg(135, 90, 3, paso);
-  pataNeg(90, 45, 6, paso);
+  pata(135, 90, 3, paso);
+  pata(90, 45, 6, paso);
   delay(3500);
 
   //retorno de los hombros 2 y 7
-  pataNeg(135, 90, 2, paso);
-  pataNeg(90, 45, 7, paso);
+  pata(135, 90, 2, paso);
+  pata(90, 45, 7, paso);
   delay(3500);
-  
+
   //Baja las patas 1 y 8
-  pataNeg(45, 0, 1, paso);
-  pataNeg(45, 0, 8, paso);
+  pata(45, 0, 1, paso);
+  pata(45, 0, 8, paso);
   delay(150);
 
   //levanta las patas 4 y 5
-  pataPos(0, 45, 4, paso);
-  pataPos(0, 45, 5, paso);
+  pata(0, 45, 4, paso);
+  pata(0, 45, 5, paso);
   delay(150);
 
   //movimiento hacia izquerda de hombros 2 y 7
-  pataPos(90, 135, 2, paso);
-  pataPos(45, 90, 7, paso);
+  pata(90, 135, 2, paso);
+  pata(45, 90, 7, paso);
   delay(3500);
 
   //retorno de los hombros 3 y 6
-  pataPos(90, 135, 3, paso);
-  pataPos(45, 90, 6, paso);
+  pata(90, 135, 3, paso);
+  pata(45, 90, 6, paso);
   delay(3500);
-  
+
   //baja las patas 4 y 5
-  pataNeg(45, 0, 4, paso);
-  pataNeg(45, 0, 5, paso);
-  delay(150);  
+  pata(45, 0, 4, paso);
+  pata(45, 0, 5, paso);
+  delay(150);
 }
 
 
 void loop() {
   swich = digitalRead(10);
   if (swich == HIGH) {
-
-    rotIzq();
-
+    movIzq();
   }
 }
